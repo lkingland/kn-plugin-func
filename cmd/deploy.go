c00aa28d6f747567167bad4de4f4c78d0afde819
Luke Kingland <luke@lukekingland.com>
2023-04-24 14:03:41 +0900

    feat: enable host builder on deploy command

--- cmd/deploy.go
+++ cmd/deploy.go
@@ -18,6 +18,7 @@ import (
 	"knative.dev/func/pkg/config"
 	fn "knative.dev/func/pkg/functions"
 	"knative.dev/func/pkg/k8s"
+	"knative.dev/func/pkg/oci"
 )
 
 func NewDeployCmd(newClient ClientFactory) *cobra.Command {
@@ -248,23 +249,26 @@ func runDeploy(cmd *cobra.Command, newClient ClientFactory) (err error) {
 
 	// Client
 	// Concrete implementations (ex builder) vary  based on final effective cfg.
-	var builder fn.Builder
-	if f.Build.Builder == builders.Pack {
-		builder = pack.NewBuilder(
+	var client *fn.Client
+	o := []fn.Option{fn.WithRegistry(cfg.Registry)}
+	if f.Build.Builder == builders.Host {
+		o = append(o,
+			fn.WithBuilder(oci.NewBuilder(builders.Host, client, cfg.Verbose)),
+			fn.WithPusher(&oci.Pusher{Verbose: cfg.Verbose}))
+	} else if f.Build.Builder == builders.Pack {
+		o = append(o, fn.WithBuilder(pack.NewBuilder(
 			pack.WithName(builders.Pack),
-			pack.WithVerbose(cfg.Verbose))
+			pack.WithVerbose(cfg.Verbose))))
 	} else if f.Build.Builder == builders.S2I {
-		builder = s2i.NewBuilder(
+		o = append(o, fn.WithBuilder(s2i.NewBuilder(
 			s2i.WithName(builders.S2I),
 			s2i.WithPlatform(cfg.Platform),
-			s2i.WithVerbose(cfg.Verbose))
+			s2i.WithVerbose(cfg.Verbose))))
 	} else {
 		return builders.ErrUnknownBuilder{Name: f.Build.Builder, Known: KnownBuilders()}
 	}
 
-	client, done := newClient(ClientConfig{Namespace: f.Deploy.Namespace, Verbose: cfg.Verbose},
-		fn.WithRegistry(cfg.Registry),
-		fn.WithBuilder(builder))
+	client, done := newClient(ClientConfig{Verbose: cfg.Verbose}, o...)
 	defer done()
 
 	// Deploy
@@ -275,10 +279,8 @@ func runDeploy(cmd *cobra.Command, newClient ClientFactory) (err error) {
 			return
 		}
 	} else {
-		if shouldBuild(cfg.Build, f, client) { // --build or "auto" with FS changes
-			if f, err = client.Build(cmd.Context(), f); err != nil {
-				return
-			}
+		if f, err = build(cmd, cfg.Build, f, client); err != nil {
+			return
 		}
 		if cfg.Push {
 			if f, err = client.Push(cmd.Context(), f); err != nil {
@@ -294,16 +296,30 @@ func runDeploy(cmd *cobra.Command, newClient ClientFactory) (err error) {
 	return f.Write()
 }
 
-// shouldBuild returns true if the value of the build option is a truthy value,
-// or if it is the literal "auto" and the function reports as being currently
-// unbuilt.  Invalid errors are not reported as this is the purview of
-// deployConfig.Validate
-func shouldBuild(buildCfg string, f fn.Function, client *fn.Client) bool {
-	if buildCfg == "auto" {
-		return !f.Built() // first build or modified filesystem
+// build when flag == 'auto' and the function is out-of-date, or when the
+// flag value is explicitly truthy such as 'true' or '1'.  Error if flag
+// is neither 'auto' nor parseable as a boolean.  Return CLI-specific error
+// message verbeage suitable for both Deploy and Run commands which feature an
+// optional build step.
+func build(cmd *cobra.Command, flag string, f fn.Function, client *fn.Client) (fn.Function, error) {
+	var err error
+	if flag == "auto" {
+		if f.Built() {
+			fmt.Fprintln(cmd.OutOrStdout(), "function up-to-date. Force rebuild with --build")
+		} else {
+			if f, err = client.Build(cmd.Context(), f); err != nil {
+				return f, err
+			}
+		}
+	} else if build, _ := strconv.ParseBool(flag); build == true {
+		if f, err = client.Build(cmd.Context(), f); err != nil {
+			return f, err
+		}
+	} else if _, err = strconv.ParseBool(flag); err != nil {
+		return f, fmt.Errorf("--build (FUNC_BUILD) %q not recognized.  Should be 'auto' or a truthy value such as 'true', 'false', '0', or '1'.", flag)
+
 	}
-	build, _ := strconv.ParseBool(buildCfg)
-	return build
+	return f, nil
 }
 
 func NewRegistryValidator(path string) survey.Validator {
